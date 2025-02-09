08f70b3945a02a44fbb1a5486b217501d3e8b66e
vandai-dong <vandaidong@gmail.com>
2025-02-09 13:59:02 +1100

    Fix toggle color when light and dark being selected; Implement dark mode for the website

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,34 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import { useState } from "react";
 import Home from "./components/Home/Home";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const themeStyles = {
+    backgroundColor: darkMode ? "#333" : "#f8f9fa",
+    color: darkMode ? "#fff" : "#333",
+    minHeight: "100vh",
+    transition: "0.3s",
+  };
+
   return (
-    <Router>
-      <NavigationBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/project" element={<Projects />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/contact" element={<Footer />} /> */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <div style={themeStyles}>
+      <Router>
+        <NavigationBar darkMode={darkMode} setDarkMode={setDarkMode} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Footer />} /> */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,24 +1,63 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import PropTypes from "prop-types";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function NavigationBar() {
+function NavigationBar({ darkMode, setDarkMode }) {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar
+      expand="lg"
+      className={`${
+        darkMode ? "bg-dark" : "bg-body-secondary"
+      }`}
+    >
       <Container>
-        <Navbar.Brand href="#home">Dai Dong</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="#home" className={darkMode ? "text-light" : "text:dark"}>Dai Dong</Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          data-bs-theme = {darkMode ? "dark" : "light"} 
+        />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="#link">Projects</Nav.Link>
-            <Nav.Link href="#link">Resume</Nav.Link>
-            <Nav.Link href="#link">Contact</Nav.Link>
+          <Nav className="ms-auto align-items-center">
+            <Nav.Link href="#home" className={darkMode ? "text-light" : "text-dark"}>Home</Nav.Link>
+            <Nav.Link href="#about" className={darkMode ? "text-light" : "text-dark"}>About</Nav.Link>
+            <Nav.Link href="#projects" className={darkMode ? "text-light" : "text-dark"}>Projects</Nav.Link>
+            <Nav.Link href="#resume" className={darkMode ? "text-light" : "text-dark"}>Resume</Nav.Link>
+            <Nav.Link href="#contact" className={darkMode ? "text-light" : "text-dark"}>Contact</Nav.Link>
+
+            <Button
+              variant={darkMode ? "dark" : "light"}
+              onClick={() => setDarkMode(!darkMode)}
+              className="ms-2"
+            >
+              {darkMode ? (
+                <img
+                  width="30"
+                  height="30"
+                  // src="https://img.icons8.com/emoji/48/crescent-moon-emoji.png"
+                  src="https://img.icons8.com/plasticine/100/bright-moon.png"
+                  alt="crescent-moon"
+                />
+              ) : (
+                <img
+                  width="30"
+                  height="30"
+                  // src="https://img.icons8.com/ios/50/sun--v1.png"
+                  src="https://img.icons8.com/plasticine/100/sun--v1.png"
+                  alt="sun"
+                />
+              )}
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
+
+// Add PropTypes validation
+NavigationBar.propTypes = {
+  darkMode: PropTypes.bool.isRequired,
+  setDarkMode: PropTypes.func.isRequired,
+};
 
 export default NavigationBar;
