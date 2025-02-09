da2d0ca0f2bb1e1f14ed8ebef2ffe6fb93789fc8
vandai-dong <vandaidong@gmail.com>
2025-02-09 11:12:09 +1100

    Add navbar collapse when resize the windown

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -7,14 +7,15 @@ function NavigationBar() {
       <Container>
         <Navbar.Brand href="#home">Dai Dong</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">About</Nav.Link>
-          <Nav.Link href="#link">Projects</Nav.Link>
-          <Nav.Link href="#link">Resume</Nav.Link>
-          <Nav.Link href="#link">Contact</Nav.Link>
-        </Nav>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Projects</Nav.Link>
+            <Nav.Link href="#link">Resume</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
